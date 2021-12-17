c56b9c309bcded4618b8a4d3001f7dd9cdeed9bd
supreethgangam <supreeth.gangam@gmail.com>
2021-12-17 21:23:33 +0530

    Update coincomb1.cpp

--- coincomb1.cpp
+++ coincomb1.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-typedef long long ll;
-const int maxN = 100;
-const int maxX = 1e6;
-const ll MOD = 1e9+7;
 
-int N, X, c[maxN];
-ll dp[maxX+1];
+int main() {
+  int mod = 1e9+7;
+  int n, target;
+  cin >> n >> target;
+  vector<int> c(n);
+  for (int&v : c) cin >> v;
 
-int main(){
-    scanf("%d %d", &N, &X);
-    for(int i = 0; i < N; i++)
-        scanf("%d", &c[i]);
-
-    dp[0] = 1;
-    for(int i = 0; i < X; i++)
-        if(dp[i] != 0)
-            for(int j = 0; j < N; j++)
-                if(i+c[j] <= X)
-                    dp[i+c[j]] = (dp[i+c[j]] + dp[i]) % MOD;
-
-    printf("%lld\n", dp[X]);
+  vector<int> dp(target+1,0);
+  dp[0] = 1;
+  for (int i = 1; i <= target; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i-c[j] >= 0) {
+	(dp[i] += dp[i-c[j]]) %= mod;
+      }
+    }
+  }
+  cout << dp[target] << endl;
 }
